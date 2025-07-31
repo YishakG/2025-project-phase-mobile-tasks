b34489adf2a692c0b48d698332ce7a1d1339a695
YishakG <yeabelec@gmail.com>
2025-07-31 15:16:08 +0300

    refactor domain layer

---
+++ on-boarding/shoe_interface/lib/domain/usecases/create_product_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import '../../core/error/failures.dart';
+import '../../core/usecases/usecase.dart';
+import '../entities/product.dart';
+import '../repositories/product_repository.dart';
+
+class CreateProductUsecase implements UseCase<Product, Product> {
+  final ProductRepository repository;
+
+  CreateProductUsecase(this.repository);
+
+  @override
+  Future<Either<Failure, Product>> call(Product product) async {
+    return await repository.createProduct(product);
+  }
+}
---
+++ on-boarding/shoe_interface/lib/domain/usecases/view_product_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import '../../core/error/failures.dart';
+import '../../core/usecases/usecase.dart';
+import '../entities/product.dart';
+import '../repositories/product_repository.dart';
+
+class ViewProductUsecase implements UseCase<Product, String> {
+  final ProductRepository repository;
+
+  ViewProductUsecase(this.repository);
+
+  @override
+  Future<Either<Failure, Product>> call(String id) async {
+    return await repository.getProduct(id);
+  }
+}
