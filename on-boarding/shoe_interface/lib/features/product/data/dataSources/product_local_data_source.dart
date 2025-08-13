0f3d8cc0710ea2580801d53246d341cb49597359
YishakG <yeabelec@gmail.com>
2025-08-13 13:39:37 +0300

    feat: local data source with tests

--- on-boarding/shoe_interface/lib/features/product/data/dataSources/product_local_data_source.dart
+++ on-boarding/shoe_interface/lib/features/product/data/dataSources/product_local_data_source.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../core/error/exceptions.dart';
 import '../models/product_model.dart';
 
 abstract class ProductLocalDataSource {
@@ -7,3 +12,54 @@ abstract class ProductLocalDataSource {
     Future<void> cacheProducts(List<ProductModel> productsToCache);
     Future<void> deleteProduct(String id);
 }
+
+class ProductLocalDataSourceImpl extends ProductLocalDataSource{
+  final productCacheKey = 'PRODUCTS';
+  final SharedPreferences sharedPreferences;
+  ProductLocalDataSourceImpl({required this.sharedPreferences});
+
+  getProductCacheKey(String id)=> '${productCacheKey}_$id';
+
+  @override
+  Future<void> cacheProduct(ProductModel product) async{
+    // TODO: implement cacheProduct
+    await sharedPreferences.setString(getProductCacheKey(product.id), jsonEncode(product.toJson()));
+  }
+
+  @override
+  Future<void> cacheProducts(List<ProductModel> productsToCache) async{
+    // TODO: implement cacheProducts
+    await sharedPreferences.setString(productCacheKey, jsonEncode(productsToCache.map((e) => e.toJson()).toList()));
+  }
+
+  @override
+  Future<void> deleteProduct(String id) {
+    // TODO: implement deleteProduct
+    return sharedPreferences.remove(getProductCacheKey(id));
+   }
+
+  @override
+  Future<ProductModel> getLastProduct(String id) async {
+    // TODO: implement getLastProduct
+    final productJson = sharedPreferences.getString(getProductCacheKey(id));
+    if (productJson != null){
+      return ProductModel.fromJson(jsonDecode(productJson));
+    }
+    else{
+      throw const CacheException(message: 'Product not found in cache');
+    }
+  }
+
+  @override
+  Future<List<ProductModel>> getLastProducts() async {
+    // TODO: implement getLastProducts
+    final productsJson = sharedPreferences.getString(productCacheKey);
+    if (productsJson != null ){
+      return (jsonDecode(productsJson) as List)
+          .map((e) => ProductModel.fromJson(e))
+          .toList();    }
+  
+  else{
+    throw const CacheException(message: 'Products not found in cache');
+  }
+}}
